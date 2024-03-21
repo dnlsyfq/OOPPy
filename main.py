0924f1cd1c57d417bb7e11a042787ad13c1acd39
Danial J <dan90@live.com.my>
2024-03-21 17:11:18 +0000

    update

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
-class MenuItem:
-	def info(self,price):
-		print(f"The price of the {self.name} is ${price}")
+from menu_item import MenuItem
 
-menu_item1 = MenuItem()
-menu_item2 = MenuItem()
-menu_item3 = MenuItem()
-menu_item4 = MenuItem()
+menu_item1 = MenuItem('Sandwich', 5)
+menu_item2 = MenuItem('Chocolate Cake', 4)
+menu_item3 = MenuItem('Coffee', 3)
+menu_item4 = MenuItem('Orange Juice', 2)
 
-menu_item1.name = 'Sandwich'
-menu_item1.info(5)
+menu_items = [menu_item1, menu_item2, menu_item3, menu_item4]
 
-menu_item2.name = 'Chocolate Cake'
-menu_item2.info(4)
+index = 0
 
-menu_item3.name = 'Coffee'
-menu_item3.info(3)
+for menu_item in menu_items:
+	print(str(index) + '. ' + menu_item.info())
+	index += 1
 
-menu_item4.name = 'Orange Juice'
-menu_item4.info(2)
+print('--------------------')
+
+order = int(input('Enter menu item number: '))
+selected_menu = menu_items[order]
+print('Selected item: ' + selected_menu.name)
+
+# Receive input from the console and set the count variable to it
+count = int(input('Enter quantity (10% off for 3 or more): '))
+
+# Call the get_total_price method
+result = selected_menu.get_total_price(count)
+
+# Output 'Your total is $____'
+print('Your total is $' + str(result))
